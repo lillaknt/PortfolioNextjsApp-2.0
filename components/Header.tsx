ed268b15cc0953e13f285b2b3a102aa788aef5aa
lillaknt <lillaknt@gmail.com>
2023-11-19 14:58:53 +0100

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return <h1>Hey, You rock!</h1>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       {/* Header */}
+      <Header />
 
       {/* Hero */}
 
