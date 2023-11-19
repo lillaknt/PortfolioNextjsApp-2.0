d52626a70c76079ad83d09389b89f2448a2305be
lillaknt <lillaknt@gmail.com>
2023-11-19 16:13:27 +0100

    Header imp.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
 export default function Header({}: Props) {
-  return <h1>Hey, You rock!</h1>;
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        {/*  Social Icons */}
+        <SocialIcon
+          url="https://linkedin.com/in/lilla-kontra/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/lillaknt"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="mailto:lillaknt@gmail.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      {/* <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className="flex flex-row items-center text-gray-300"
+        >
+          <SocialIcon
+            url="mailto:lillaknt@gmail.com"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      </Link> */}
+    </header>
+  );
 }
