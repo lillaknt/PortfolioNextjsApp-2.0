c3e5e4ba2ebcc9625d8fecc6054df5f9e614305a
lillaknt <lillaknt@gmail.com>
2023-11-19 19:00:15 +0100

    Bug fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,10 @@ export default function Projects({}: Props) {
       <h3 className="sectionTitle">Projects</h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-offGray scrollbar-thumb-brownPrimary/20">
         {projects.map((project, i) => (
-          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen ">
+          <div
+            key={i}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
             <motion.img
               initial={{
                 y: -10,
@@ -38,7 +41,6 @@ export default function Projects({}: Props) {
               src="/images/komorebi.png"
               alt=""
             />
-
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
                 <span className="underline decoration-turqAccent/50">
